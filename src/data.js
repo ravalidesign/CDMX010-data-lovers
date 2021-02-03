c5c067e0fd6be8172ba9c6324f2b397ab667162a
ravalidesign <ravalidesign@gmail.com>
2021-02-02 19:40:12 -0600

    proyecto pokemon

--- src/data.js
+++ src/data.js
@@ -1,9 +1,100 @@
-// estas funciones son de ejemplo
+import data from "./data/pokemon/pokemon.js";
 
-export const example = () => {
-  return 'example';
-};
+export const PokemonCard = (pokemon) => {
+    return `
+    <div class="flip-container">
+    <div class="flipper">
+    <div class="card">
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+    <div class="cabecera">
+    <h4>${pokemon.num}</h4>
+    <p></p>
+    <h2>${pokemon.name}</h2>
+    </div>
+
+    <div class="card-container">
+        <img src="${pokemon.img}" alt="" aling="left" class="img-fluid">
+        <div class="tipo">
+        <h3>Tipo: </h3>
+        <p>${pokemon.type}</p>
+        <h3> size: </h3>
+        <p>${pokemon.size.height}</p>
+        <p>${pokemon.size.weight}</p>
+        </div>
+        <h3>Acerca de:</h3>
+        <p> ${pokemon.about}</p> 
+        </div>
+
+    </div>
+
+    <div class="back">
+    <div class="card-back-container">
+    <div class="cabecera">
+    <h4>${pokemon.num}</h4>
+    <p></p>
+    <h2>${pokemon.name}</h2>
+    </div>
+    <div class="card-container">
+    <div class:"size">
+    
+    <div class"resdeb">
+    <h3> resistant </h3>
+        <p> ${pokemon.resistant}</p>
+    <h3> weaknesses </h3>
+        <p> ${pokemon.weaknesses}</p>
+    </div>
+   
+    <h3> stats </h3>
+        <p>base-attack <p>
+        <p> ${pokemon.stats["base-attack"]}</p>
+        <p>base-defense <p>
+        <p> ${pokemon.stats["base-defense"]}</p>
+        <p>base-stamina <p>
+        <p> ${pokemon.stats["base-stamina"]}</p>
+        <p>max-cp<p>
+        <p> ${pokemon.stats["max-cp"]}</p>
+        <p>max-hp<p>
+        <p> ${pokemon.stats["max-hp"]}</p>
+
+    <h3> evolution </h3>
+        <p> ${pokemon.evolution["next-evolution"] ? pokemon.evolution["next-evolution"][0]["name"] : 
+        pokemon.evolution["prev-evolution"] ? pokemon.evolution["prev-evolution"][0]["name"] : 
+        "No tiene evolución"} </p>
+        
+
+
+        </div> 
+    </div>
+   </div>
+        
+    </div>
+    </div>
+    </div>
+    `
+}
+
+
+// función para mostrar tarjetas
+
+export const showPokemons = (pokemons) => {
+    let pokemonsCards = ''
+
+    pokemons.forEach(pokemon => {
+        pokemonsCards += PokemonCard(pokemon)
+     })
+
+     tarjetas.innerHTML = pokemonsCards;
+}
+showPokemons(data.pokemon)
+
+
+
+
+
+// función de filtro
+export const filterByType=(type)=>{
+    const filterType=data.pokemon.filter(poke =>{
+        return poke.type.includes(type)
+    })
+       return filterType;
+    }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,89 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import pokemon from "./data/pokemon/pokemon.js";
+import data from "./data/pokemon/pokemon.js";
+import {PokemonCard,showPokemons,filterByType} from "./data.js"
 
-console.log(example, data);
+
+let tarjetas = document.getElementById("tarjetas")
+let tarjets="";
+const tipo=document.getElementById("tipo");
+const poder = document.getElementById("poder");
+const aparicion = document.getElementById("aparicion");
+const formulario= document.getElementById("formulario");
+const busqueda= document.getElementById("busqueda");
+
+//search
+
+const datosUsuario= data.pokemon.forEach(e => {
+    formulario.addEventListener("keyup" , (e) => {
+       e.preventDefault;
+
+       let letra= busqueda.value.toLowerCase();
+       console.log(letra);
+
+       const pokemonsFiltrados = data.pokemon.filter(item => {
+           const api = item.name.toLowerCase();
+           return api.indexOf(letra) !== -1
+       })
+       
+       showPokemons(pokemonsFiltrados)
+   })
+});
+
+//Filtrado por tipo
+
+const pokemonType= data.pokemon.forEach(e=>{
+    tipo.addEventListener( "click" , e =>{
+    let filtradoTipo = e.target.value;
+     let filterType =filterByType(filtradoTipo)   
+     showPokemons(filterType) 
+    })   
+})
+
+
+    // mas poderosos
+
+
+const poderpokemon = data.pokemon.forEach(e => {
+    poder.addEventListener("click", (e) => {
+        const maxHp= data.pokemon.sort((prev,next)=>{
+
+    return next.stats["max-cp"] - prev.stats["max-cp"]
+    
+   })
+   showPokemons(maxHp)
+    })
+});
+
+   
+ 
+// Orden de Aparición 
+
+const ordenapa = data.pokemon.forEach(e => {
+    aparicion.addEventListener("click", (e) => {
+       const encuentro = data.pokemon.sort((prev,next) => {
+
+    return next.encounter["base-capture-rate"] - prev.encounter["base-capture-rate"]
+    
+   })
+   showPokemons(encuentro) 
+    })
+});
+
+let evoluciones = data.pokemon;
+// console.log(evoluciones);
+evoluciones.forEach(e => {
+ e.evolution["next-evolution"] ? console.log(e.evolution["next-evolution"][0]["name"]) : 
+e.evolution["prev-evolution"] ? console.log(e.evolution["prev-evolution"][0]["name"]) : 
+console.log("No tiene evolución")})
+
+
+
+
+
+ 
+
+
+
+
+
+
